22be75ed0de46576dfea8abe5c5958bceb9b62ba
SNN <snndmrprx@gmail.com>
2022-12-17 19:14:13 +0300

    Solutions added.

--- Solutions.cpp
+++ Solutions.cpp
@@ -1,31 +1,25 @@
 #include "Solutions.h"
 
-int Solutions::findCenter(std::vector<std::vector<int>>& edges)
-{
-	if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
-	{
+int Solutions::findCenter(std::vector<std::vector<int>>& edges) {
+	if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
 		return edges[0][0];
 	}
 
 	return edges[0][1];
 }
 
-int Solutions::sumOddLengthSubarrays(std::vector<int>& arr)
-{
+int Solutions::sumOddLengthSubarrays(std::vector<int>& arr) {
 	const size_t size = arr.size();
 	std::vector<int> preSum(size + 1);
 
-	for (size_t i = 0; i < size; i++)
-	{
+	for (size_t i = 0; i < size; i++) {
 		preSum[i + 1] = preSum[i] + arr[i];
 	}
 
 	int result = 0;
 
-	for (size_t i = 0; i < size; i++)
-	{
-		for (size_t j = 0; j < size - i; j += 2)
-		{
+	for (size_t i = 0; i < size; i++) {
+		for (size_t j = 0; j < size - i; j += 2) {
 			result += preSum[i + j + 1] - preSum[i];
 		}
 	}
@@ -33,26 +27,21 @@ int Solutions::sumOddLengthSubarrays(std::vector<int>& arr)
 	return result;
 }
 
-int Solutions::deleteGreatestValue(std::vector<std::vector<int>>& grid)
-{
+int Solutions::deleteGreatestValue(std::vector<std::vector<int>>& grid) {
 	const size_t gridSize = grid.size();
 	const size_t rowSize = grid.at(0).size();
 
-	for (size_t i = 0; i < gridSize; i++)
-	{
+	for (size_t i = 0; i < gridSize; i++) {
 		std::sort(grid[i].begin(), grid[i].end());
 	}
 
 	int result = 0;
 
-	for (size_t i = 0; i < rowSize; i++)
-	{
+	for (size_t i = 0; i < rowSize; i++) {
 		int max = 0;
 
-		for (size_t j = 0; j < gridSize; j++)
-		{
-			if (grid[j][i] > max)
-			{
+		for (size_t j = 0; j < gridSize; j++) {
+			if (grid[j][i] > max) {
 				max = grid[j][i];
 			}
 		}
@@ -63,8 +52,7 @@ int Solutions::deleteGreatestValue(std::vector<std::vector<int>>& grid)
 	return result;
 }
 
-bool Solutions::arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2)
-{
+bool Solutions::arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2) {
 	std::string concanated1;
 	std::string concanated2;
 
@@ -76,10 +64,67 @@ bool Solutions::arrayStringsAreEqual(std::vector<std::string>& word1, std::vecto
 		concanated2 += piece;
 	}
 
-	if (concanated1.size() != concanated2.size())
-	{
+	if (concanated1.size() != concanated2.size()) {
 		return false;
 	}
 
 	return concanated1 == concanated2;
+}
+
+int Solutions::numberOfMatches(int n) {
+	int sum = 0;
+
+	while (n > 1) {
+		if (n % 2 == 0) {
+			sum += n / 2;
+			n /= 2;
+		} else {
+			sum += (n - 1) / 2;
+			n /= 2;
+			n++;
+		}
+	}
+
+	return sum;
+}
+
+int Solutions::uniqueMorseRepresentations(std::vector<std::string>& words) {
+	std::string morseAlphabet[26] = {
+			".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+			".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+			"...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--.." };
+
+	std::unordered_set<std::string> convertedWords;
+	std::vector<std::string> converted(words.size(), "");
+
+	for (std::string& word : words) {
+		std::string convertedWord;
+
+		for (char& letter : word) {
+			convertedWord += morseAlphabet[letter - 'a'];
+		}
+
+		convertedWords.insert(convertedWord);
+	}
+
+	return convertedWords.size();
+}
+
+int Solutions::maxDepth(std::string s) {
+	int maxDepth = 0;
+	int currentDepth = maxDepth;
+
+	for (char& c : s) {
+		if (c == '(') {
+			currentDepth++;
+		} else if (c == '(') {
+			currentDepth--;
+		}
+
+		if (currentDepth > maxDepth) {
+			maxDepth = currentDepth;
+		}
+	}
+
+	return maxDepth;
 }
--- Solutions.h
+++ Solutions.h
@@ -3,9 +3,9 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <unordered_set>
 
-class Solutions
-{
+class Solutions {
 	public:
 	/// <summary>No: 51</summary>
 	/// <see href="https://leetcode.com/problems/find-center-of-star-graph/">1791. Find Center of Star Graph</see>
@@ -22,5 +22,17 @@ class Solutions
 	/// <summary>No: 54</summary>
 	/// <see href="https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/">1662. Check If Two String Arrays are Equivalent</see>
 	static bool arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2);
+
+	/// <summary>No: 55</summary>
+	/// <see href="https://leetcode.com/problems/count-of-matches-in-tournament/">1688. Count of Matches in Tournament</see>
+	static int numberOfMatches(int n);
+
+	/// <summary>No: 56</summary>
+	/// <see href="https://leetcode.com/problems/unique-morse-code-words/">804. Unique Morse Code Words</see>
+	static int uniqueMorseRepresentations(std::vector<std::string>& words);
+
+	/// <summary>No: 57</summary>
+	/// <see href="https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/">1614. Maximum Nesting Depth of the Parentheses</see>
+	static int maxDepth(std::string s);
 };
 
